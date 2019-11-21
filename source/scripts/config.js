a8b0a2716434022016f0c5bd9a31039c27c58a2e
Junior Moraes <juniorfvox@gmail.com>
2019-11-21 00:46:14 -0200

    Include path on api url

--- source/scripts/config.js
+++ source/scripts/config.js
@@ -1,7 +1,7 @@
 export default {
   api: {
     domain: (window.location.hostname.indexOf('observatoriodaprimeirainfancia') !== -1
-      ? 'https://omlpi-api.appcivico.com/'
-      : 'https://dev-omlpi-api.appcivico.com/'),
+      ? 'https://omlpi-api.appcivico.com/v1/'
+      : 'https://dev-omlpi-api.appcivico.com/v1/'),
   },
 };
