677751f7e5e496e8841c6d6ca07cdb046d42af55
eduruiz <contato@eduruiz.com.br>
2020-10-06 11:39:36 -0300

    Update config js to have multiple production domains

--- assets/assets/scripts/index.js
+++ assets/assets/scripts/index.js
@@ -9271,16 +9271,17 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
+var productionDomains = ['rnpiobserva.org.br', 'inspiring-heisenberg-e2220d.netlify.app'];
 var _default = {
   api: {
-    domain: window.location.hostname.indexOf('observatoriodaprimeirainfancia') !== -1 ? 'https://omlpi-api.appcivico.com/v1/' : 'https://dev-omlpi-api.appcivico.com/v1/',
-    docs: window.location.hostname.indexOf('observatoriodaprimeirainfancia') !== -1 ? 'https://omlpi-docs.appcivico.com/' : 'https://dev-omlpi-docs.appcivico.com/'
+    domain: productionDomains.indexOf(window.location.hostname) > -1 ? 'https://omlpi-api.appcivico.com/v1/' : 'https://dev-omlpi-api.appcivico.com/v1/',
+    docs: productionDomains.indexOf(window.location.hostname) > -1 ? 'https://omlpi-docs.appcivico.com/' : 'https://dev-omlpi-docs.appcivico.com/'
   },
   apiCMS: {
-    domain: window.location.hostname.indexOf('observatoriodaprimeirainfancia') !== -1 ? 'https://omlpi-strapi.appcivico.com/' : 'https://dev-omlpi-strapi.appcivico.com/'
+    domain: productionDomains.indexOf(window.location.hostname) > -1 ? 'https://omlpi-strapi.appcivico.com/' : 'https://dev-omlpi-strapi.appcivico.com/'
   },
   storage: {
-    domain: window.location.hostname.indexOf('observatoriodaprimeirainfancia') !== -1 ? 'https://omlpi-strapi.appcivico.com/' : 'https://dev-omlpi-strapi.appcivico.com/'
+    domain: productionDomains.indexOf(window.location.hostname) > -1 ? 'https://omlpi-strapi.appcivico.com/' : 'https://dev-omlpi-strapi.appcivico.com/'
   },
   fisrtCityId: 5200050
 };
