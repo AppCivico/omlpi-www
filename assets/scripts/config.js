8f281accf8debb202795671b959ae4e70ea184eb
sobral <sobral@appcivico.com>
2022-10-11 11:17:39 -0300

    Improve indentation

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,20 +4,25 @@ export default {
   api: {
     domain: (productionDomains.indexOf(window.location.hostname) > -1
       ? 'https://omlpi-api.appcivico.com/v2/'
-      : 'https://dev-omlpi-api.appcivico.com/v2/'),
+      // : 'https://dev-omlpi-api.appcivico.com/v2/'
+      : 'https://omlpi-api.appcivico.com/v2/'
+    ),
     docs: (productionDomains.indexOf(window.location.hostname) > -1
       ? 'https://omlpi-docs.appcivico.com/'
-      : 'https://dev-omlpi-docs.appcivico.com/'),
+      : 'https://omlpi-docs.appcivico.com/'
+    ),
   },
   apiCMS: {
     domain: (productionDomains.indexOf(window.location.hostname) > -1
       ? 'https://omlpi-strapi.appcivico.com/'
-      : 'https://omlpi-strapi.appcivico.com/'),
+      : 'https://omlpi-strapi.appcivico.com/'
+    ),
   },
   storage: {
     domain: (productionDomains.indexOf(window.location.hostname) > -1
       ? 'https://omlpi-strapi.appcivico.com'
-      : 'https://omlpi-strapi.appcivico.com'),
+      : 'https://omlpi-strapi.appcivico.com'
+    ),
   },
   firstCityId: 5200050,
 };
