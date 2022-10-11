928bc1c18adc396adc021afcdd919396a8181466
sobral <sobral@appcivico.com>
2022-10-11 15:06:57 -0300

    Update configs per environment

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,21 +1,24 @@
-const productionDomains = ['rnpiobserva.org.br', 'inspiring-heisenberg-e2220d.netlify.app'];
+const productionDomains = [
+  'rnpiobserva.org.br',
+  'dev.rnpiobserva.org.br',
+  'inspiring-heisenberg-e2220d.netlify.app',
+];
 
 export default {
   api: {
     domain: (productionDomains.indexOf(window.location.hostname) > -1
       ? 'https://omlpi-api.appcivico.com/v2/'
-      // : 'https://dev-omlpi-api.appcivico.com/v2/'
-      : 'https://omlpi-api.appcivico.com/v2/'
+      : 'https://dev-omlpi-api.appcivico.com/v2/'
     ),
     docs: (productionDomains.indexOf(window.location.hostname) > -1
       ? 'https://omlpi-docs.appcivico.com/'
-      : 'https://omlpi-docs.appcivico.com/'
+      : '/'
     ),
   },
   apiCMS: {
     domain: (productionDomains.indexOf(window.location.hostname) > -1
       ? 'https://omlpi-strapi.appcivico.com/'
-      : 'https://omlpi-strapi.appcivico.com/'
+      : 'http://localhost:1337/'
     ),
   },
   storage: {
